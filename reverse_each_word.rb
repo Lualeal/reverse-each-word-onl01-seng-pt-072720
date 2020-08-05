7989d2073a91dea43cae4820d847252dbe7c0017
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 17:14:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -15,3 +15,14 @@ def reverse_each_word_with_collect(string)
  end
   another_array.join(" ")
 end
+
+
+
+def reverse_each_word_with_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
